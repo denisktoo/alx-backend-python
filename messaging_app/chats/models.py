2ee389e7775cfe656bea7ea87fba4225cc7d1b5e
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-15 23:38:40 +0300

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -16,6 +16,7 @@ class User(AbstractUser):
         return self.email
 
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -23,6 +24,7 @@ class Conversation(models.Model):
         return f"Conversation {self.id}"
 
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     message_body = models.TextField()
