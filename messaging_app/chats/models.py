977352b3d8c0cd311a7c7dede126576fc1fadd1b
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-07-16 00:46:17 +0300

    Run the application to fix errors

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
-    phone_number = models.CharField(max_length=20)
-    role = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
